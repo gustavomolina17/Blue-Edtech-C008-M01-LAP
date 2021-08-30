03fccb027d952b5a44b819e8f45d793754105ad4
Gustavo Molina <gustavo.molina17@yahoo.com.br>
2021-08-30 20:38:41 -0300

    create dowhile2.js

---
+++ Aula 10/dowhile2.js
@@ -0,0 +1,38 @@
+// Exemplo 01 - While
+
+/*let resultado = "";
+let g = 0;
+
+while (g < 8) {
+  g += 1; // g = g+1
+  resultado += g; // resultado = resultado + g
+  console.log(resultado);
+} */
+
+// Exemplo 2 - DO .... WHILE:
+// Faça... Enquanto
+
+let resultado2 = "";
+let i = 0;
+
+do {
+  // Ações do meu programa
+
+  i = i + 1; // i++
+
+  resultado2 = resultado2 + i;
+} while (i < 5);
+
+console.log("Resultado do DO WHILE: " + resultado2);
+
+// Exemplo 03
+
+let resultado3 = "";
+let x = 1;
+
+while (x < 5) {
+  resultado3 = resultado3 + x;
+  x = x + 1; // i++
+}
+
+console.log("Resultado do WHILE: " + resultado3);
