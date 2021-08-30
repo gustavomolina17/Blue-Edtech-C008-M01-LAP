24a97c040ddc9987c433bfa266a03be50f0fff55
Gustavo Molina <gustavo.molina17@yahoo.com.br>
2021-08-30 16:37:28 -0300

    Create função_anonima

---
+++ Aula 11/funcao_anonima.js
@@ -0,0 +1,27 @@
+// Uma função anônima é uma função sem nome.
+
+//Uma função anônima geralmente não está acessível após a sua criação.
+
+//Exemplo 01 -> Como precisamos chamar a função anônima em algum momento, atribuímos essa função a uma variável, nesse caso à variável show.
+
+let show = function () {
+  console.log("Esse é um exemplo de função anônima");
+};
+
+show();
+
+//Costumamos usar funções anônimas como argumentos de outras funções.
+
+// Exemplo 02 -> Passamos por argumento uma função anônima para a função setTimeout(). A função anônima passa a ser então um parâmetro da função setTimeout. A função setTimeout() executa essa função anônima um segundo depois.
+
+setTimeout(function () {
+  console.log("Função executada após 1s");
+}, 1000);
+
+//Se você deseja criar uma função e executá-la imediatamente após a declaração, pode usar a função anônima parecida com o Exemplo 03.
+
+// Exemplo 03
+
+(function () {
+  console.log("Função executada após a declaração");
+})();
